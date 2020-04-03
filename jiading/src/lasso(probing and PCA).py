97048bcfa9a8880da47e5c116ce3f7af98f94485
JiaDingCN <jiading.biz@outlook.com>
2020-04-03 21:53:39 +0800

    add PCA

---
+++ jiading/src/lasso(probing and PCA).py
@@ -0,0 +1,14 @@
+import loadData as load
+import outPutResult as out
+from sklearn.linear_model import Lasso
+from sklearn.decomposition import PCA
+pca=PCA(n_components=15)
+x_train,y_train,x_test=load.loadData()
+pca.fit(x_train)
+x_train=pca.transform(x_train)
+pca.fit(x_test)
+x_test=pca.transform(x_test)
+lasso = Lasso(alpha=0.031, tol=0.01, random_state=213, selection='random')
+lasso.fit(x_train,y_train)
+prediction=lasso.predict(x_test)
+out.outPut(prediction,"lasso(probing and PCA)15features")
